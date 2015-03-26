f5046657105a63443d8d601353d0895fa8fc6825
MajeedMirza <MajeedMirza@users.noreply.github.com>
2015-03-26 02:54:16 -0400

    Update MyButtonListener.java

--- MyButtonListener.java
+++ MyButtonListener.java
@@ -2,9 +2,7 @@ import java.awt.event.*;
 public class MyButtonListener implements ActionListener
 {
     private GradeCalculator g;
-    /**
-     * Constructor for objects of class MyButtonListener
-     */
+    
     public MyButtonListener(GradeCalculator g)
     {
         this.g = g;
